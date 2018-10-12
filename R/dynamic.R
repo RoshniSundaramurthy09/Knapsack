2eb949280f8a2dceb8f1ee3394a02b1ba0adc2a7
RoshniSundaramurthy09 <43231073+RoshniSundaramurthy09@users.noreply.github.com>
2018-10-12 13:52:48 +0200

    Add files via upload

--- R/dynamic.R
+++ R/dynamic.R
@@ -17,17 +17,17 @@ knapsack_dynamic <- function(x, W){
   n <- nrow(x)
   w <- x[[1]]  #weights
   v <- x[[2]]  #values
-  cap <- W     #capacity of knapsack
+  capacity <- W     #capacity of knapsack
   elem <- c()
-  m <- matrix(0,nrow = n+1,ncol = cap+1)
+  m <- matrix(0,nrow = n+1,ncol = capacity+1)
  
   #to return the max value 
   for (i in 2:n){
-    for (j in 1:cap){
-      if (w[i] > j) 
-        m[i,j] <- m[i-1, j]
+    for (j in 1:capacity){
+      if (j > w[i]) 
+        m[i, j] <- max(m[i-1, j-w[i]] + v[i], m[i-1, j])
       else 
-        m[i, j] <- max(m[i-1, j], m[i-1, j-w[i]] + v[i])
+        m[i,j] <- m[i-1, j]
     }
   }
   
@@ -35,10 +35,10 @@ knapsack_dynamic <- function(x, W){
   value <- round(val)
   
   #to print the elements
-  while(cap>0 && n-1>0){
-    if(m[n,cap]!=m[n-1,cap]){
+  while(capacity>0 && n-1>0){
+    if(m[n,capacity]!=m[n-1,capacity]){
       elem<-c(elem,n)
-      cap<-cap-w[n]
+      capacity<-capacity-w[n]
       elements <- sort(elem)
     }
     n<-n-1
@@ -59,8 +59,10 @@ knapsack_objects <-
 #system.time({knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)})
 
 #run time of a chunk of code
-start.time <- Sys.time()
+library(tictoc)
+tic("sleeping")   # refer https://www.r-bloggers.com/5-ways-to-measure-running-time-of-r-code/
+print("falling asleep...")
 knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)
-end.time <- Sys.time()
-end.time - start.time
+print("...waking up")
+toc()
 
